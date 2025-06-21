2e8f7e93275f2dadfa461dce3519989aa187bc0d
Mohsinali00 <135958631+Mohsinali00@users.noreply.github.com>
2025-06-21 23:11:39 +0530

    updated_this_code

---
+++ working/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
+from .views import home,HOME,DATA,signup,login,about_logout,userdata_view,logout
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('HOME',HOME,name='HOME1'),
+    path('DATA',DATA,name='DATA'),
+    path('signup',signup,name='signup'),
+    path('login',login,name='login'),
+    path('about',about_logout,name='login'),
+    path('datainserted',userdata_view,name='datainserted'),
+    path('logout',logout,name='logout'),
+
+
+
+    
+]
